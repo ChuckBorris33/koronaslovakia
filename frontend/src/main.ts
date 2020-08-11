a07d8030082dd098c462ea44179ee2b04507b0cf
Boris Zalman <boris.zalman@qvanel.com>
2020-08-11 22:26:14 +0200

    Modify svelte template

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,11 @@
-import App from './App.svelte';
+import App from "./App.svelte";
+
+import "./styles/global.scss";
+import "bootstrap";
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {},
 });
 
 export default app;
