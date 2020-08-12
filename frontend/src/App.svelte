f0d702570f060b25a604b8bebbb5b0d15eae52d2
Boris Zalman <boris.zalman@qvanel.com>
2020-08-12 22:25:31 +0200

    Add basic navigation

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,16 +1,27 @@
 <svelte:head>
 	<title>Korona</title>
 	<link rel='icon' type='image/x-icon' href='/favicon.ico'>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </svelte:head>
 
 <script lang="ts">
+	import { Container, Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'sveltestrap'
+	import Icon from 'fa-svelte'
+	import { faGithub } from '@fortawesome/free-brands-svg-icons'
+	import ChartList from './components/ChartList.svelte'
 </script>
 
-<main>
-	<span>works</span>
-</main>
-
-<style type="text/scss">
-
-</style>
+<Navbar toggleable="lg" class="navbar-dark bg-primary">
+	<Container>
+		<NavbarBrand href="#">Koronavírus na slovensku</NavbarBrand>
+		<Nav class="ml-auto" navbar>
+			<NavItem>
+				<NavLink href="https://github.com/ChuckBorris33/koronaslovakia">
+					<Icon icon="{ faGithub }"></Icon>
+				</NavLink>
+			</NavItem>
+		</Nav>
+	</Container>
+</Navbar>
+<Container class="main">
+	<ChartList/>
+</Container>
---
+++ frontend/src/components/ChartList.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { Col, Nav, NavItem, NavLink, Row } from "sveltestrap";
+
+  const charts: {
+    title: string;
+    component?: any;
+    id: string;
+  }[] = [
+    {
+      title: "Súhrn",
+      id: "summary",
+    },
+  ];
+</script>
+
+<Row class="w-100">
+  <Col xs="3" class="d-none d-md-block">
+    <Nav id="main-navigation" vertical>
+      {#each charts as chart, index}
+        <NavItem>
+          <NavLink class="nav-link" active={index === 0} href={`#${chart.id}`}>
+            {chart.title}
+          </NavLink>
+        </NavItem>
+      {/each}
+    </Nav>
+  </Col>
+  <Col md="9" xs="12">
+    {#each charts as chart}
+      {#if chart.component}
+        <svelte:component this={chart.component} id={chart.id} />
+      {/if}
+    {/each}
+  </Col>
+</Row>
